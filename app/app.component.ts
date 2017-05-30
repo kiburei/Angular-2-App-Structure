64db695942145b95ac0e1b28a69e8435e4548ff8
kiburei <akiburei@gmail.com>
2017-05-30 17:18:42 +0300

    To Do Angular app

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,44 +5,43 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3>One of my favorite bands is: {{ favoriteBand }}</h3>
-    <p>If I had to choose a favorite painter it would be: {{ favoritePainter }}</p>
-    <p>The number of slices of pie I would like is: {{ slicesOfPie }}</p>
-    <h3>One of my favorite albums is: </h3>
-    <p>{{ favoriteAlbum.title }}</p>
-    <p>By {{ favoriteAlbum.artist }}</p>
-    <p>Released in {{ favoriteAlbum.released }}</p>
-    <h3>Here are my favorite pies!</h3>
-    <div class="pie" *ngFor="let currentPie of favoritePies">
-      <p>{{currentPie}}</p>
+    <div *ngFor="let currentTask of tasks">
+      <h3 >{{ currentTask.description }}</h3>
+      <button (click)="showDetails(currentTask)" >Edit</button>
     </div>
-    <h3>Here are my favorite albums!</h3>
-    <div class="album" *ngFor="let album of albums">
-      <p>{{album.title}}</p>
-      <p>By {{album.artist}}</p>
-      <p>Released in {{album.released}}</p>
+    <div *ngIf="selectedTask">
+      <h1>Edit Task</h1>
+      <div>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+      </div>
+      <div>
+        <label>Enter Task ID:</label>
+        <input [(ngModel)]="selectedTask.id">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
 })
 
 export class AppComponent {
-  favoriteBand: string = 'Ani DiFranco';
-  favoritePainter: string = 'Van Gogh';
-  slicesOfPie: number = 3;
-  favoriteAlbum: Album = new Album("Disintegration", "The Cure", 1989);
-  favoritePies: string[] = ["Apple", "Banana Cream", "Blackberry"];
-  albums: Album[] = [
-       new Album("Pulse", "Pink Floyd", 1995),
-       new Album("Funhouse", "The Stooges", 1970),
-       new Album("Twilight of the Thunder God", "Amon Amarth", 2008),
-       new Album("Dilate", "Ani DiFranco", 1996),
-       new Album("Chopin - Complete Nocturnes", "Brigitte Engerer", 2015),
-       new Album("Axis Bold As Love", "The Jimi Hendrix Experience", 1967)
-     ];
-
+  public tasks: Task[] = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+  ];
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task){
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing(){
+    this.selectedTask = null;
+  }
 }
 
-export class Album {
-  constructor(public title: string, public artist: string, public released: number){}
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {   }
 }
